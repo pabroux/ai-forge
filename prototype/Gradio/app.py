c2992aadacfe3ff7c12cb4f13bf0608224a71216
pabroux <pierre-alexandrebroux@live.fr>
2024-06-03 16:09:59 +0200

    feat: add gradio prototype example.

---
+++ prototype/Gradio/app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * intensity
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", gr.Slider(value=2, minimum=1, maximum=10, step=1)],
+    outputs=[gr.Textbox(label="greeting", lines=3)],
+)
+
+demo.queue(
+    default_concurrency_limit=5,
+)
+
+demo.launch()
+
